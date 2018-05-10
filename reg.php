f560aae4680c3dca46686b887ba6d08f7eb1eca3
Alex <pro_ak48@mail.ru>
2018-05-10 17:53:53 +0300

    Update reg.php

--- reg.php
+++ reg.php
@@ -3,11 +3,11 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/components/User.php';
 if (filter_has_var(INPUT_POST, 'data')) {
 
     $result = array(
-        'login' = filter_input(INPUT_POST, 'login', FILTER_SANITIZE_SPECIAL_CHARS),
+        'login' => filter_input(INPUT_POST, 'login', FILTER_SANITIZE_SPECIAL_CHARS),
         'password' => $_POST["password"],
         'confirm_password' => $_POST["confirm_password"],
         'email' => $_POST["email"],
-        'name' = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS)
+        'name' => filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS)
     );
 
     $answer = User::register(json_encode($result));
